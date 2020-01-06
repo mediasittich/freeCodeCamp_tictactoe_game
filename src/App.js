2969fdd24d188b7d0474488ffe1bcb4c482c0bf6
Regina Galambos <gina.galambos@gmail.com>
2020-01-06 15:42:01 +0100

    add game ui html & styles

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <div className="game-board">
+                <div className="box">X</div>
+                <div className="box"></div>
+                <div className="box">X</div>
+                <div className="box"></div>
+                <div className="box">O</div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box">O</div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
